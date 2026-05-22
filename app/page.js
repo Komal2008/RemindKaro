db1ba902f5959eadaaca856d1a533c0c5d822cec
Kartik Pant <kartikpant.kp69@gmail.com>
2026-05-23 01:23:21 +0530

    chore: rename page.html to page.js

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import LandingClient from '@/components/LandingClient';
+import { cookies } from 'next/headers';
+
+export default async function LandingPage() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get('auth_token')?.value;
+  const isLoggedIn = !!token;
+
+  return
+  <LandingClient isLoggedIn={isLoggedIn} />;
+}
